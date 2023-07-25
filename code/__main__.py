c96287495a1bdce2e179fa804c7520300a3258b1
XinxieWu <xinxiewu@gmail.com>
2023-07-25 19:35:30 -0400

    EDA1

---
+++ code/__main__.py
@@ -0,0 +1,60 @@
+"""
+__maim__.py contains the workflow to run all sub-programs
+"""
+import os
+from util import *
+from base_models import *
+from deep_learning import *
+from imbalanced_strategy import *
+
+def main(fielurl=None, output=r'../public/output', drop_col=None, target=None, feat_disc_threshold=None, gpu_yn=False):
+    """
+    Step 1: Data Preparation & EDA
+    """
+    # 1.1 Download dataset from Github & read as DataFrame
+    df = download_file(fielurl)
+    if drop_col is not None:
+        df.drop(columns=drop_col, inplace=True)
+    # 1.2 EDA
+    # (a) Basic Statistic, Missing & Outlier/Extreme Values
+    df.describe().to_csv(os.path.join(output, 'basic_statistic.csv'))
+    ## Distribution of Features (by threshold) & Targets
+
+
+    # (b) Data Normalization
+
+    # (c) Correlation Matrix
+
+    # (d) Feature Engineering/Selection
+
+    """
+    Step 2: Base Models - Confusion Matrix & AUC-ROC
+    """
+
+    """
+    Step 3: Improvement by Feature Engineering
+    """
+
+    """
+    Step 4: Imbalance Discussion
+    """
+
+    """
+    Step 5: Deep Learning
+    """
+
+    return
+
+if __name__ == '__main__':
+    """
+    Step 1: Clean output folder
+    """
+    delete_files(path=r'../public/output')
+    """
+    Step 2: Call the main program
+    """
+    # main(fielurl = 'https://raw.githubusercontent.com/xinxiewu/datasets/main/credit_card_default/default_of_credit_card_clients.csv',
+    #      drop_col= ['ID'],
+    #      target  = 'default_payment_next_month',
+    #      feat_disc_threshold = 11,
+    #      gpu_yn = True)
--- code/util.py
+++ code/util.py
@@ -1,22 +1,25 @@
 """
 util.py contains custom functions:
     1. download_file: Download the .csv file from the given link and read as dataframe
+    2. delete_files: Delete files in the given folder, except README.md
 """
 
 import requests
 import pandas as pd
+import os
 
 # download_file(url)
-def download_file(url=None):
+def download_file(url=None, output=r'../public/output'):
     """ Download the .csv file from the given link and read as dataframe
 
     Args: 
         url: str
+        output: path to store downloaded files
     
     Returns:
         DataFrame
     """
-    local_filename = url.split('/')[-1]
+    local_filename = os.path.join(output, url.split('/')[-1])
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
         with open(local_filename, 'wb') as f:
@@ -24,3 +27,18 @@ def download_file(url=None):
                 f.write(chunk)
     return pd.read_csv(local_filename)
 
+# delete_files(path)
+def delete_files(path=None, keep=['README.md']):
+    """ Delete files in the given folder path, except README.md
+
+    Args:
+        path: path, starting with r''
+        keep: files to keep, default value as README.md
+
+    Returns:
+        nothing to return
+    """
+    for fname in os.listdir(path):
+        if fname not in (keep):
+            os.remove(os.path.join(path, fname))
+    return
